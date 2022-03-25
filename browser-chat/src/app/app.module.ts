9b24da60cd0539987f3a4912ae4603bb35ec19ed
Jorge Calderon <calderon.rjorge@gmail.com>
2022-03-25 16:25:38 -0400

    frontend layout

---
+++ browser-chat/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from "@angular/forms";
+
+
+// components
+import { ChatComponent } from './components/chat/chat.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
